e4be30d848a20002af60cb11c36f0e03aa4ce4ae
Ashishchaursiya <chaurasiyaashish383@gmail.com>
2024-04-11 16:30:55 +0530

    role added

--- src/api/routers/routeIndex.js
+++ src/api/routers/routeIndex.js
@@ -1,12 +1,13 @@
 const express = require('express');
 const authRoutes = require('./authRoute');
 const aminityRoutes = require('./aminityRoute');
-
+const roleRoutes = require('./roleRoute');
 
 const allRouters = express.Router();
 
 
 allRouters.use("/auth", authRoutes);
 allRouters.use("/aminity", aminityRoutes);
+allRouters.use("/role", roleRoutes);
 
 module.exports =  allRouters;
