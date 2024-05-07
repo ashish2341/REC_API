ce62ed879a38d249ea343c2ca8f64958f902a290
Alka Sinha <sinhaalka802211@gmail.com>
2024-05-07 13:12:49 +0530

    Filter Done

--- src/api/validators/commonValidator.js
+++ src/api/validators/commonValidator.js
@@ -2,7 +2,8 @@ const Joi = require('joi');
 const getRecordsSchema = Joi.object({
     page: Joi.number().integer().min(1).required(),
     pageSize: Joi.number().integer().min(1).required(),
-    search: Joi.string().allow('')
+    search: Joi.string().allow(''),
+    filter:Joi.string().allow(''),
   });
   const idSchema = Joi.object({
     id: Joi.string().required().length(24).error(new Error('Id is inValid')),
