b5a5e3aa77a4e4b47aed56f44a5e3372349579df
Alka Sinha <sinhaalka802211@gmail.com>
2024-05-15 11:38:12 +0530

    done

--- src/api/validators/zodiacValidator.js
+++ src/api/validators/zodiacValidator.js
@@ -2,8 +2,8 @@ const Joi = require('joi');
 
 const AddZodiacSchema = Joi.object({
     Name: Joi.string().required(),
-    MobileNumber:Joi.number(),
-    DateOfBirth: Joi.date(),
+    MobileNumber:Joi.number().required(),
+    DateOfBirth: Joi.date().required(),
     
 });
 
